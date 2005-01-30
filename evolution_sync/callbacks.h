dcafe8f1c3e9029880a2bc5ceb950908f14f97ac
abauer <abauer@53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e>
2005-01-30 23:37:50 +0000

    Added gui for the evolution_sync plugin
    git-svn-id: http://svn.opensync.org/multisync/trunk/gui@82 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

---
+++ evolution_sync/callbacks.h
@@ -0,0 +1,15 @@
+#include <gnome.h>
+
+
+gboolean
+evo_evooptwin_delete_event               (GtkWidget       *widget,
+                                        GdkEvent        *event,
+                                        gpointer         user_data);
+
+void
+evo_evocancelbutton_clicked              (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+evo_evookbutton_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
