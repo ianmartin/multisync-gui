f9b15d9f90293c33c2373c17846f5212fc33b8f0
dbackx <dbackx@53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e>
2005-10-09 13:55:47 +0000

    Learning how to develop with Gtk, and I'm slowly getting somewhere :-)
    git-svn-id: http://svn.opensync.org/multisync/trunk/gui@135 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

--- synce_sync/interface.c
+++ synce_sync/interface.c
@@ -26,17 +26,7 @@
 #define GLADE_HOOKUP_OBJECT_NO_REF(component,widget,name) \
   g_object_set_data (G_OBJECT (component), name, widget)
 
-static GnomeUIInfo e2convertwidget1_uiinfo[] =
-{
-  GNOMEUIINFO_END
-};
-
-static GnomeUIInfo e2menu1_uiinfo[] =
-{
-  GNOMEUIINFO_END
-};
-
-static GnomeUIInfo e2convertwidget2_uiinfo[] =
+static GnomeUIInfo dir_submenu_uiinfo[] =
 {
   GNOMEUIINFO_END
 };
@@ -49,38 +39,21 @@ create_synce_wnd_options (void)
   GtkWidget *vbox2;
   GtkWidget *label15;
   GtkWidget *hbox1;
-  GtkWidget *label12;
   GtkWidget *table1;
-  GtkWidget *label13;
   GtkWidget *button1;
   GtkWidget *alignment1;
   GtkWidget *hbox2;
   GtkWidget *image1;
   GtkWidget *label14;
   GtkWidget *txt_path;
-  GtkWidget *label16;
-  GtkWidget *hbox3;
-  GtkWidget *label17;
-  GtkWidget *table2;
-  GtkWidget *lbl_passwd;
-  GtkWidget *chk_mode;
-  GtkWidget *txt_mode;
-  GtkWidget *e2label4;
+  GtkWidget *optionmenu1;
+  GtkWidget *dir_submenu;
   GtkWidget *e2hbox1;
   GtkWidget *e2label5;
-  GtkWidget *e2calendarmenu;
-  AtkObject *atko;
-  GtkWidget *e2convertwidget1;
-  GtkWidget *e2label6;
   GtkWidget *e2hbox2;
   GtkWidget *e2label7;
-  GtkWidget *e2addressbookmenu;
-  GtkWidget *e2menu1;
-  GtkWidget *e2label8;
   GtkWidget *e2hbox3;
   GtkWidget *e2label9;
-  GtkWidget *e2todomenu;
-  GtkWidget *e2convertwidget2;
   GtkWidget *hseparator10;
   GtkWidget *hbuttonbox2;
   GtkWidget *btn_cancel;
@@ -106,7 +79,7 @@ create_synce_wnd_options (void)
   gtk_box_pack_start (GTK_BOX (vbox1), vbox2, TRUE, TRUE, 0);
   gtk_container_set_border_width (GTK_CONTAINER (vbox2), 6);
 
-  label15 = gtk_label_new ("<span weight='bold'>Options</span>");
+  label15 = gtk_label_new ("<span weight='bold'>Enter the directory to synchronize</span>");
   gtk_widget_set_name (label15, "label15");
   gtk_widget_show (label15);
   gtk_box_pack_start (GTK_BOX (vbox2), label15, FALSE, FALSE, 0);
@@ -119,30 +92,17 @@ create_synce_wnd_options (void)
   gtk_widget_show (hbox1);
   gtk_box_pack_start (GTK_BOX (vbox2), hbox1, FALSE, TRUE, 0);
 
-  label12 = gtk_label_new ("    ");
-  gtk_widget_set_name (label12, "label12");
-  gtk_widget_show (label12);
-  gtk_box_pack_start (GTK_BOX (hbox1), label12, FALSE, FALSE, 0);
-
-  table1 = gtk_table_new (1, 4, FALSE);
+  table1 = gtk_table_new (1, 3, FALSE);
   gtk_widget_set_name (table1, "table1");
   gtk_widget_show (table1);
   gtk_box_pack_start (GTK_BOX (hbox1), table1, TRUE, TRUE, 0);
   gtk_table_set_row_spacings (GTK_TABLE (table1), 6);
   gtk_table_set_col_spacings (GTK_TABLE (table1), 12);
 
-  label13 = gtk_label_new_with_mnemonic ("Directory");
-  gtk_widget_set_name (label13, "label13");
-  gtk_widget_show (label13);
-  gtk_table_attach (GTK_TABLE (table1), label13, 0, 1, 0, 1,
-                    (GtkAttachOptions) (GTK_FILL),
-                    (GtkAttachOptions) (0), 0, 0);
-  gtk_misc_set_alignment (GTK_MISC (label13), 0, 0.5);
-
   button1 = gtk_button_new ();
   gtk_widget_set_name (button1, "button1");
   gtk_widget_show (button1);
-  gtk_table_attach (GTK_TABLE (table1), button1, 3, 4, 0, 1,
+  gtk_table_attach (GTK_TABLE (table1), button1, 2, 3, 0, 1,
                     (GtkAttachOptions) (GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
 
@@ -161,7 +121,7 @@ create_synce_wnd_options (void)
   gtk_widget_show (image1);
   gtk_box_pack_start (GTK_BOX (hbox2), image1, FALSE, FALSE, 0);
 
-  label14 = gtk_label_new_with_mnemonic ("Browse");
+  label14 = gtk_label_new_with_mnemonic ("List directories");
   gtk_widget_set_name (label14, "label14");
   gtk_widget_show (label14);
   gtk_box_pack_start (GTK_BOX (hbox2), label14, FALSE, FALSE, 0);
@@ -169,67 +129,22 @@ create_synce_wnd_options (void)
   txt_path = gtk_entry_new ();
   gtk_widget_set_name (txt_path, "txt_path");
   gtk_widget_show (txt_path);
-  gtk_table_attach (GTK_TABLE (table1), txt_path, 1, 3, 0, 1,
+  gtk_table_attach (GTK_TABLE (table1), txt_path, 0, 2, 0, 1,
                     (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
   gtk_widget_set_size_request (txt_path, 40, 23);
 
-  label16 = gtk_label_new ("<span weight='bold'>Default Mode:</span>");
-  gtk_widget_set_name (label16, "label16");
-  gtk_widget_show (label16);
-  gtk_box_pack_start (GTK_BOX (vbox2), label16, FALSE, FALSE, 0);
-  gtk_label_set_use_markup (GTK_LABEL (label16), TRUE);
-  gtk_label_set_justify (GTK_LABEL (label16), GTK_JUSTIFY_CENTER);
-  gtk_label_set_line_wrap (GTK_LABEL (label16), TRUE);
+  optionmenu1 = gtk_option_menu_new ();
+  gtk_widget_set_name (optionmenu1, "optionmenu1");
+  gtk_widget_show (optionmenu1);
+  gtk_box_pack_start (GTK_BOX (vbox2), optionmenu1, FALSE, FALSE, 0);
 
-  hbox3 = gtk_hbox_new (FALSE, 0);
-  gtk_widget_set_name (hbox3, "hbox3");
-  gtk_widget_show (hbox3);
-  gtk_box_pack_start (GTK_BOX (vbox2), hbox3, TRUE, TRUE, 0);
+  dir_submenu = gtk_menu_new ();
+  gtk_widget_set_name (dir_submenu, "dir_submenu");
+  gnome_app_fill_menu (GTK_MENU_SHELL (dir_submenu), dir_submenu_uiinfo,
+                       accel_group, FALSE, 0);
 
-  label17 = gtk_label_new ("    ");
-  gtk_widget_set_name (label17, "label17");
-  gtk_widget_show (label17);
-  gtk_box_pack_start (GTK_BOX (hbox3), label17, FALSE, FALSE, 0);
-
-  table2 = gtk_table_new (1, 3, FALSE);
-  gtk_widget_set_name (table2, "table2");
-  gtk_widget_show (table2);
-  gtk_box_pack_start (GTK_BOX (hbox3), table2, TRUE, TRUE, 0);
-  gtk_table_set_row_spacings (GTK_TABLE (table2), 6);
-  gtk_table_set_col_spacings (GTK_TABLE (table2), 12);
-
-  lbl_passwd = gtk_label_new ("Default Mode");
-  gtk_widget_set_name (lbl_passwd, "lbl_passwd");
-  gtk_widget_show (lbl_passwd);
-  gtk_table_attach (GTK_TABLE (table2), lbl_passwd, 0, 1, 0, 1,
-                    (GtkAttachOptions) (GTK_FILL),
-                    (GtkAttachOptions) (0), 0, 0);
-  gtk_label_set_line_wrap (GTK_LABEL (lbl_passwd), TRUE);
-  gtk_misc_set_alignment (GTK_MISC (lbl_passwd), 0, 0.5);
-
-  chk_mode = gtk_check_button_new_with_mnemonic ("Overwrite file mode");
-  gtk_widget_set_name (chk_mode, "chk_mode");
-  gtk_widget_show (chk_mode);
-  gtk_table_attach (GTK_TABLE (table2), chk_mode, 2, 3, 0, 1,
-                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
-                    (GtkAttachOptions) (0), 0, 0);
-
-  txt_mode = gtk_entry_new ();
-  gtk_widget_set_name (txt_mode, "txt_mode");
-  gtk_widget_show (txt_mode);
-  gtk_table_attach (GTK_TABLE (table2), txt_mode, 1, 2, 0, 1,
-                    (GtkAttachOptions) (GTK_FILL),
-                    (GtkAttachOptions) (0), 0, 0);
-  gtk_widget_set_size_request (txt_mode, 38, -1);
-  gtk_entry_set_text (GTK_ENTRY (txt_mode), "664");
-
-  e2label4 = gtk_label_new ("<span weight='bold'>Calendar:</span>");
-  gtk_widget_set_name (e2label4, "e2label4");
-  gtk_widget_show (e2label4);
-  gtk_box_pack_start (GTK_BOX (vbox1), e2label4, FALSE, FALSE, 0);
-  gtk_label_set_use_markup (GTK_LABEL (e2label4), TRUE);
-  gtk_misc_set_alignment (GTK_MISC (e2label4), 0, 0.5);
+  gtk_option_menu_set_menu (GTK_OPTION_MENU (optionmenu1), dir_submenu);
 
   e2hbox1 = gtk_hbox_new (FALSE, 0);
   gtk_widget_set_name (e2hbox1, "e2hbox1");
@@ -241,25 +156,6 @@ create_synce_wnd_options (void)
   gtk_widget_show (e2label5);
   gtk_box_pack_start (GTK_BOX (e2hbox1), e2label5, FALSE, FALSE, 0);
 
-  e2calendarmenu = gtk_option_menu_new ();
-  gtk_widget_set_name (e2calendarmenu, "e2calendarmenu");
-  gtk_widget_show (e2calendarmenu);
-  gtk_box_pack_start (GTK_BOX (e2hbox1), e2calendarmenu, TRUE, TRUE, 0);
-
-  e2convertwidget1 = gtk_menu_new ();
-  gtk_widget_set_name (e2convertwidget1, "e2convertwidget1");
-  gnome_app_fill_menu (GTK_MENU_SHELL (e2convertwidget1), e2convertwidget1_uiinfo,
-                       accel_group, FALSE, 0);
-
-  gtk_option_menu_set_menu (GTK_OPTION_MENU (e2calendarmenu), e2convertwidget1);
-
-  e2label6 = gtk_label_new ("<span weight='bold'>Addressbook:</span>");
-  gtk_widget_set_name (e2label6, "e2label6");
-  gtk_widget_show (e2label6);
-  gtk_box_pack_start (GTK_BOX (vbox1), e2label6, FALSE, FALSE, 0);
-  gtk_label_set_use_markup (GTK_LABEL (e2label6), TRUE);
-  gtk_misc_set_alignment (GTK_MISC (e2label6), 0, 0.5);
-
   e2hbox2 = gtk_hbox_new (FALSE, 0);
   gtk_widget_set_name (e2hbox2, "e2hbox2");
   gtk_widget_show (e2hbox2);
@@ -270,25 +166,6 @@ create_synce_wnd_options (void)
   gtk_widget_show (e2label7);
   gtk_box_pack_start (GTK_BOX (e2hbox2), e2label7, FALSE, FALSE, 0);
 
-  e2addressbookmenu = gtk_option_menu_new ();
-  gtk_widget_set_name (e2addressbookmenu, "e2addressbookmenu");
-  gtk_widget_show (e2addressbookmenu);
-  gtk_box_pack_start (GTK_BOX (e2hbox2), e2addressbookmenu, TRUE, TRUE, 0);
-
-  e2menu1 = gtk_menu_new ();
-  gtk_widget_set_name (e2menu1, "e2menu1");
-  gnome_app_fill_menu (GTK_MENU_SHELL (e2menu1), e2menu1_uiinfo,
-                       accel_group, FALSE, 0);
-
-  gtk_option_menu_set_menu (GTK_OPTION_MENU (e2addressbookmenu), e2menu1);
-
-  e2label8 = gtk_label_new ("<span weight='bold'>Tasks:</span>");
-  gtk_widget_set_name (e2label8, "e2label8");
-  gtk_widget_show (e2label8);
-  gtk_box_pack_start (GTK_BOX (vbox1), e2label8, FALSE, FALSE, 0);
-  gtk_label_set_use_markup (GTK_LABEL (e2label8), TRUE);
-  gtk_misc_set_alignment (GTK_MISC (e2label8), 0, 0.5);
-
   e2hbox3 = gtk_hbox_new (FALSE, 0);
   gtk_widget_set_name (e2hbox3, "e2hbox3");
   gtk_widget_show (e2hbox3);
@@ -298,18 +175,6 @@ create_synce_wnd_options (void)
   gtk_widget_set_name (e2label9, "e2label9");
   gtk_widget_show (e2label9);
   gtk_box_pack_start (GTK_BOX (e2hbox3), e2label9, FALSE, FALSE, 0);
-
-  e2todomenu = gtk_option_menu_new ();
-  gtk_widget_set_name (e2todomenu, "e2todomenu");
-  gtk_widget_show (e2todomenu);
-  gtk_box_pack_start (GTK_BOX (e2hbox3), e2todomenu, TRUE, TRUE, 0);
-
-  e2convertwidget2 = gtk_menu_new ();
-  gtk_widget_set_name (e2convertwidget2, "e2convertwidget2");
-  gnome_app_fill_menu (GTK_MENU_SHELL (e2convertwidget2), e2convertwidget2_uiinfo,
-                       accel_group, FALSE, 0);
-
-  gtk_option_menu_set_menu (GTK_OPTION_MENU (e2todomenu), e2convertwidget2);
 
   hseparator10 = gtk_hseparator_new ();
   gtk_widget_set_name (hseparator10, "hseparator10");
@@ -345,55 +210,27 @@ create_synce_wnd_options (void)
                     G_CALLBACK (synce_on_btn_ok_clicked),
                     NULL);
 
-  gtk_label_set_mnemonic_widget (GTK_LABEL (label13), txt_path);
-
-  atko = gtk_widget_get_accessible (e2calendarmenu);
-  atk_object_set_description (atko, "Select the calendar you wish to sync.");
-
-  atko = gtk_widget_get_accessible (e2addressbookmenu);
-  atk_object_set_description (atko, "Select the addressbook you wish to sync.");
-
-  atko = gtk_widget_get_accessible (e2todomenu);
-  atk_object_set_description (atko, "Select the tasks you wish to sync.");
-
-
   /* Store pointers to all widgets, for use by lookup_widget(). */
   GLADE_HOOKUP_OBJECT_NO_REF (synce_wnd_options, synce_wnd_options, "synce_wnd_options");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, vbox1, "vbox1");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, vbox2, "vbox2");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, label15, "label15");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, hbox1, "hbox1");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, label12, "label12");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, table1, "table1");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, label13, "label13");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, button1, "button1");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, alignment1, "alignment1");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, hbox2, "hbox2");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, image1, "image1");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, label14, "label14");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, txt_path, "txt_path");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, label16, "label16");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, hbox3, "hbox3");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, label17, "label17");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, table2, "table2");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, lbl_passwd, "lbl_passwd");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, chk_mode, "chk_mode");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, txt_mode, "txt_mode");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2label4, "e2label4");
+  GLADE_HOOKUP_OBJECT (synce_wnd_options, optionmenu1, "optionmenu1");
+  GLADE_HOOKUP_OBJECT (synce_wnd_options, dir_submenu, "dir_submenu");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, e2hbox1, "e2hbox1");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, e2label5, "e2label5");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2calendarmenu, "e2calendarmenu");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2convertwidget1, "e2convertwidget1");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2label6, "e2label6");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, e2hbox2, "e2hbox2");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, e2label7, "e2label7");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2addressbookmenu, "e2addressbookmenu");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2menu1, "e2menu1");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2label8, "e2label8");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, e2hbox3, "e2hbox3");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, e2label9, "e2label9");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2todomenu, "e2todomenu");
-  GLADE_HOOKUP_OBJECT (synce_wnd_options, e2convertwidget2, "e2convertwidget2");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, hseparator10, "hseparator10");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, hbuttonbox2, "hbuttonbox2");
   GLADE_HOOKUP_OBJECT (synce_wnd_options, btn_cancel, "btn_cancel");
--- synce_sync/synce_sync.c
+++ synce_sync/synce_sync.c
@@ -1,6 +1,3 @@
-/*
- * Stolen from evolution2
- */
 #include "multisync.h"
 #include "interface.h"
 #include "callbacks.h"
@@ -8,13 +5,12 @@
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
 
+#include <gtk/gtk.h>
+
 GtkWidget *synce_wnd_options;
 
 typedef struct synce_options {
-	char		*addressbook_path;
-	char		*calendar_path;
-	char		*tasks_path;
-	char		*path;
+	char		*file_path;
 	osync_bool	recursive;
 } synce_options;
 
@@ -23,124 +19,31 @@ synce_options *options;
 GtkWidget *synce_wnd_options;
 OSyncMember *member;
 
-void synce_fill_calendar_menu(OSyncMember *member, char *selected)
+void synce_fill_directory_menu(OSyncMember *member, char *selected)
 {
-#if 0
-	GtkWidget *menuitem;
-	GtkWidget *calendarmenu;
-	calendarmenu = gtk_menu_new ();
-	
-	menuitem = gtk_menu_item_new_with_label ("No Calendar");
-	gtk_menu_append (GTK_MENU (calendarmenu), menuitem);
-	gtk_object_set_data (GTK_OBJECT (menuitem), "uri", NULL);
-	gtk_menu_item_activate(GTK_MENU_ITEM(menuitem));
-	
-	OSyncError *error = NULL;
-	GList *calendars = osync_member_call_plugin(member, "evo2_list_calendars", NULL, &error);
-	if (osync_error_is_set(&error)) {
-		printf("Unable to call plugin: %s\n", osync_error_print(&error));
-		osync_error_free(&error);
-		return;
-	}
-	
-	GList *c;
-	int n = 1;
-	for (c = calendars; c; c = c->next) {
-		evo2_location *path = c->data;
-		menuitem = gtk_menu_item_new_with_label(path->name);
-		gtk_menu_append (GTK_MENU (calendarmenu), menuitem);
-		gtk_object_set_data (GTK_OBJECT (menuitem), "uri", path->uri);
-		//FIXME free the evo2_location somewhere!
-		if (selected && !strcmp(path->uri, selected)) {
-			gtk_menu_item_activate(GTK_MENU_ITEM(menuitem));
-			gtk_menu_set_active (GTK_MENU(calendarmenu), n);
-		}
-		n++;
-	}
-			
-	gtk_option_menu_set_menu (GTK_OPTION_MENU(lookup_widget(synce_wnd_options, "calendarmenu")), calendarmenu);
-	gtk_widget_show_all(GTK_WIDGET(calendarmenu));
-#endif
-}
+	GList		*d, *dirs;
+	OSyncError	*error = NULL;
+	GtkWidget	*menu = gtk_menu_new();
+	GtkWidget	*menuitem;
 
-void synce_fill_tasks_menu(OSyncMember *member, char *selected)
-{
-#if 0
-	GtkWidget *menuitem;
-	GtkWidget *tasksmenu;
-	tasksmenu = gtk_menu_new ();
-	
-	menuitem = gtk_menu_item_new_with_label ("No Tasks");
-	gtk_menu_append (GTK_MENU (tasksmenu), menuitem);
-	gtk_object_set_data (GTK_OBJECT (menuitem), "uri", NULL);
-	gtk_menu_item_activate(GTK_MENU_ITEM(menuitem));
-	
-	OSyncError *error = NULL;
-	GList *tasks = osync_member_call_plugin(member, "evo2_list_tasks", NULL, &error);
-	if (osync_error_is_set(&error)) {
-		printf("Unable to call plugin: %s\n", osync_error_print(&error));
-		osync_error_free(&error);
-		return;
-	}
-	
-	GList *c;
-	int n = 1;
-	for (c = tasks; c; c = c->next) {
-		evo2_location *path = c->data;
-		menuitem = gtk_menu_item_new_with_label(path->name);
-		gtk_menu_append (GTK_MENU (tasksmenu), menuitem);
-		gtk_object_set_data (GTK_OBJECT (menuitem), "uri", path->uri);
-		//FIXME free the evo2_location somewhere!
-		if (selected && !strcmp(path->uri, selected)) {
-			gtk_menu_item_activate(GTK_MENU_ITEM(menuitem));
-			gtk_menu_set_active (GTK_MENU(tasksmenu), n);
-		}
-		n++;
-	}
-			
-	gtk_option_menu_set_menu (GTK_OPTION_MENU(lookup_widget(synce_wnd_options, "todomenu")), tasksmenu);
-	gtk_widget_show_all(GTK_WIDGET(tasksmenu));
-#endif
-}
+	fprintf(stderr, "%s\n", __func__);
 
-void synce_fill_addressbook_menu(OSyncMember *member, char *selected)
-{
+	dirs = osync_member_call_plugin(member, "synce_list_directories", NULL, &error);
+
+	for (d = dirs; d; d = d->next) {
+		char	*s = d->data;
 #if 0
-	GtkWidget *menuitem;
-	GtkWidget *addressbookmenu;
-	addressbookmenu = gtk_menu_new ();
-	
-	menuitem = gtk_menu_item_new_with_label ("No Addressbook");
-	gtk_menu_append (GTK_MENU (addressbookmenu), menuitem);
-	gtk_object_set_data (GTK_OBJECT (menuitem), "uri", NULL);
-	gtk_menu_item_activate(GTK_MENU_ITEM(menuitem));
-	
-	OSyncError *error = NULL;
-	GList *tasks = osync_member_call_plugin(member, "evo2_list_addressbooks", NULL, &error);
-	if (osync_error_is_set(&error)) {
-		printf("Unable to call plugin: %s\n", osync_error_print(&error));
-		osync_error_free(&error);
-		return;
-	}
-	
-	GList *c;
-	int n = 1;
-	for (c = tasks; c; c = c->next) {
-		evo2_location *path = c->data;
-		menuitem = gtk_menu_item_new_with_label(path->name);
-		gtk_menu_append (GTK_MENU (addressbookmenu), menuitem);
-		gtk_object_set_data (GTK_OBJECT (menuitem), "uri", path->uri);
-		//FIXME free the evo2_location somewhere!
-		if (selected && !strcmp(path->uri, selected)) {
-			gtk_menu_item_activate(GTK_MENU_ITEM(menuitem));
-			gtk_menu_set_active (GTK_MENU(addressbookmenu), n);
-		}
-		n++;
-	}
-			
-	gtk_option_menu_set_menu (GTK_OPTION_MENU(lookup_widget(synce_wnd_options, "addressbookmenu")), addressbookmenu);
-	gtk_widget_show_all(GTK_WIDGET(addressbookmenu));
+		fprintf(stderr, "%s -> %s\n", __func__, s);
 #endif
+		menuitem = gtk_menu_item_new_with_label(s);
+		gtk_menu_append(GTK_MENU(menu), menuitem);
+		gtk_object_set_data(GTK_OBJECT(menuitem), "uri", s);
+		gtk_menu_item_activate(GTK_MENU_ITEM(menuitem));
+	}
+
+	gtk_option_menu_set_menu(GTK_OPTION_MENU(lookup_widget(synce_wnd_options,
+					"optionmenu1")), menu);
+	gtk_widget_show_all(GTK_WIDGET(menu));
 }
 
 void msync_synce_make_config(synce_options *options, char **data, int *size)
@@ -150,10 +53,8 @@ void msync_synce_make_config(synce_options *options, char **data, int *size)
 	doc = xmlNewDoc((xmlChar*)"1.0");
 	doc->children = xmlNewDocNode(doc, NULL, (xmlChar*)"config", NULL);
 	
-	xmlNewChild(doc->children, NULL, (xmlChar*)"adress_path", (xmlChar*)options->addressbook_path);
-	xmlNewChild(doc->children, NULL, (xmlChar*)"calendar_path", (xmlChar*)options->calendar_path);
-	xmlNewChild(doc->children, NULL, (xmlChar*)"tasks_path", (xmlChar*)options->tasks_path);
-	
+	xmlNewChild(doc->children, NULL, (xmlChar*)"files_path", (xmlChar*)options->file_path);
+
 	xmlDocDumpMemory(doc, (xmlChar **)data, size);
 	*size++;
 }
@@ -166,7 +67,7 @@ static osync_bool fs_parse_settings(synce_options *env, char *data, int size, OS
 	xmlNodePtr cur;
 
 	//set defaults
-	env->path = "";
+	env->file_path = "";
 	env->recursive = TRUE;
 
 	doc = xmlParseMemory(data, size);
@@ -198,8 +99,8 @@ static osync_bool fs_parse_settings(synce_options *env, char *data, int size, OS
 	while (cur != NULL) {
 		char *str = (char*)xmlNodeGetContent(cur);
 		if (str) {
-			if (!xmlStrcmp(cur->name, (const xmlChar *)"path")) {
-				env->path = g_strdup(str);
+			if (!xmlStrcmp(cur->name, (const xmlChar *)"files_path")) {
+				env->file_path = g_strdup(str);
 			}
 			if (!xmlStrcmp(cur->name, (const xmlChar *)"recursive")) {
 				env->recursive = (g_ascii_strcasecmp(str, "TRUE") == 0);
@@ -218,19 +119,17 @@ osync_bool msync_synce_parse_config(synce_options *options, char *data, int size
 {
 	xmlDocPtr doc;
 	xmlNodePtr cur;
-	printf("EVO2-SYNC start: %s\n", __func__);
+	printf("SynCE-SYNC start: %s\n", __func__);
 
 	//set defaults
-	options->addressbook_path = NULL;
-	options->calendar_path = NULL;
-	options->tasks_path = NULL;
+	options->file_path = NULL;
 
 	printf("Config data: %s\n", data);
 
 	doc = xmlParseMemory(data, size);
 
 	if (!doc) {
-		printf("EVO2-SYNC Could not parse data!\n");
+		printf("SynCE-SYNC Could not parse data!\n");
 		msync_show_msg_warn("Unable to open options.\nCould not parse configuration data");
 		return FALSE;
 	}
@@ -238,13 +137,13 @@ osync_bool msync_synce_parse_config(synce_options *options, char *data, int size
 	cur = xmlDocGetRootElement(doc);
 
 	if (!cur) {
-		printf("EVO2-SYNC data seems to be empty\n");
+		printf("SynCE-SYNC data seems to be empty\n");
 		xmlFreeDoc(doc);
 		return FALSE;
 	}
 
 	if (xmlStrcmp(cur->name, (xmlChar*)"config")) {
-		printf("EVO2-SYNC data seems not to be a valid configdata.\n");
+		printf("SynCE-SYNC data seems not to be a valid configdata.\n");
 		xmlFreeDoc(doc);
 		return FALSE;
 	}
@@ -254,14 +153,8 @@ osync_bool msync_synce_parse_config(synce_options *options, char *data, int size
 	while (cur != NULL) {
 		char *str = (char*)xmlNodeGetContent(cur);
 		if (str) {
-			if (!xmlStrcmp(cur->name, (const xmlChar *)"adress_path")) {
-				options->addressbook_path = g_strdup(str);
-			}
-			if (!xmlStrcmp(cur->name, (const xmlChar *)"calendar_path")) {
-				options->calendar_path = g_strdup(str);
-			}
-			if (!xmlStrcmp(cur->name, (const xmlChar *)"tasks_path")) {
-				options->tasks_path = g_strdup(str);	
+			if (!xmlStrcmp(cur->name, (const xmlChar *)"files_path")) {
+				options->file_path = g_strdup(str);
 			}
 			xmlFree(str);
 		}
@@ -298,11 +191,9 @@ void msync_synce_sync_options(MSyncEnv *env, OSyncMember *target)
 	if (!fs_parse_settings(options, config, size, &error))
 		return;
 	
-	synce_fill_calendar_menu(target, options->calendar_path);
-	synce_fill_tasks_menu(target, options->tasks_path);
-	synce_fill_addressbook_menu(target, options->addressbook_path);
+	synce_fill_directory_menu(target, options->file_path);
 	
-	gtk_widget_show (synce_wnd_options);
+	gtk_widget_show(synce_wnd_options);
 	g_free(config);
 #if 0
 	GtkEntry *entry = GTK_ENTRY(lookup_widget(synce_wnd_options, "txt_path"));
@@ -330,22 +221,16 @@ synce_on_btn_cancel_clicked            (GtkButton       *button,
 
 
 void
-synce_on_btn_ok_clicked                (GtkButton       *button,
-                                        gpointer         user_data)
+synce_on_btn_ok_clicked(GtkButton *button, gpointer user_data)
 {
 	GtkWidget *item;
-	item = gtk_option_menu_get_menu (GTK_OPTION_MENU (lookup_widget(synce_wnd_options, "addressbookmenu")));
-	item = gtk_menu_get_active (GTK_MENU (item));
-	options->addressbook_path = gtk_object_get_data (GTK_OBJECT (item), "uri");
-	
-	item = gtk_option_menu_get_menu (GTK_OPTION_MENU (lookup_widget(synce_wnd_options, "calendarmenu")));
-	item = gtk_menu_get_active (GTK_MENU (item));
-	options->calendar_path = gtk_object_get_data (GTK_OBJECT (item), "uri");
-	
-	item = gtk_option_menu_get_menu (GTK_OPTION_MENU (lookup_widget(synce_wnd_options, "todomenu")));
-	item = gtk_menu_get_active (GTK_MENU (item));
-	options->tasks_path = gtk_object_get_data (GTK_OBJECT (item), "uri");
-	
+
+	item = gtk_option_menu_get_menu(GTK_OPTION_MENU
+			(lookup_widget(synce_wnd_options, "optionmenu1")));
+	item = gtk_menu_get_active(GTK_MENU(item));
+	options->file_path = gtk_object_get_data(GTK_OBJECT (item), "uri");
+	fprintf(stderr, "Yow %s(%s)\n", __func__, options->file_path);
+
 	char *config = NULL;
 	int size = 0;
 	msync_synce_make_config(options, &config, &size);
